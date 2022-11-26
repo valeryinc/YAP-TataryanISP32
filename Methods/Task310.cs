eb4e15af7cd95cc52b8e65fbef1fcd3eac110252
Valery <94569372+valeryinc@users.noreply.github.com>
2022-11-26 23:23:32 +0400

    бульмени

--- Methods/Task310.cs
+++ Methods/Task310.cs
@@ -6,13 +6,21 @@ class Task310 : ISolution
     public void GetSolution()
     {
         int[] a = { 3, 2, 1, 5, 3 };
-        int[] b = { 6, 2, 21, 24, };
+        int[] b = { 6, 2, 21, 24 };
 
-        Console.WriteLine(ArraySum(a, b));
+        ArraySum(a, b);
     }
 
-    public int ArraySum(int[] a, int[] b)
+    public int[] ArraySum(int[] a, int[] b)
     {
-        return a.Sum() + b.Sum();
+        int[] n = new int[a.Length > b.Length ? a.Length : b.Length];
+
+        for (int i = 0; i < a.Length; ++i)
+            n[i] += a[i];
+
+        for (int i = 0; i < b.Length; ++i)
+            n[i] += b[i];
+
+        return n;
     }
 }
